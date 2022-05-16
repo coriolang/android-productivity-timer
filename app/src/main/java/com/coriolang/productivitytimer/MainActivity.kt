9c9ad4c349f7cc404142aadc2fd459fac189189d
coriolang <yan.radi@gmail.com>
2022-05-16 19:16:59 +0300

    Progress bar

--- app/src/main/java/com/coriolang/productivitytimer/MainActivity.kt
+++ app/src/main/java/com/coriolang/productivitytimer/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.coriolang.productivitytimer
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ProgressBar
 import androidx.activity.viewModels
 import com.coriolang.productivitytimer.databinding.ActivityMainBinding
 import com.coriolang.productivitytimer.viewModels.MainViewModel
@@ -11,6 +14,18 @@ class MainActivity : AppCompatActivity() {
     private val viewModel: MainViewModel by viewModels()
     private lateinit var binding: ActivityMainBinding
 
+    private val colors: IntArray = intArrayOf(
+        Color.RED,
+        Color.BLUE,
+        Color.CYAN,
+        Color.GRAY,
+        Color.GREEN,
+        Color.DKGRAY,
+        Color.YELLOW,
+        Color.LTGRAY,
+        Color.MAGENTA
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -19,14 +34,27 @@ class MainActivity : AppCompatActivity() {
 
         binding.startButton.setOnClickListener {
             viewModel.startStopwatch()
+            binding.progressBar.visibility = ProgressBar.VISIBLE
         }
 
         binding.resetButton.setOnClickListener {
             viewModel.resetStopwatch()
+            binding.progressBar.visibility = ProgressBar.INVISIBLE
         }
+
+        var lastColor = colors.random()
 
         viewModel.stopwatchString.observe(this) {
             binding.textView.text = it
+
+            var currentColor = colors.random()
+            while (currentColor == lastColor) {
+                currentColor = colors.random()
+            }
+            lastColor = currentColor
+
+            binding.progressBar.indeterminateTintList =
+                ColorStateList.valueOf(currentColor)
         }
     }
 }
